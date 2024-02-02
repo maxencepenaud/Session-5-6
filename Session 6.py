2f0913cc14c8d7fd4c6b60496d9eec0c065ed03b
maxencepenaud <mpenaud.ieu2022@student.ie.edu>
2024-02-02 10:47:19 +0100

    hello

--- Session 6.py
+++ Session 6.py
@@ -1,2 +1,6 @@
 name = input("What is your name")
-print("Hello",name,"!", sep="")
+age = input("How old are you")
+# print("Hello",name,"!", sep="")
+age = int(age)  # convert string to integer
+birth_year = 2024 - age
+print(name, ",you were born in", birth_year, ".", sep="")
